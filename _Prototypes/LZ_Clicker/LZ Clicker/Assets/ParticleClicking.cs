278731c7fba5406d688f9e869bdc06e2ca056a1d
Jordan Cazamias <jacazamias@gmail.com>
2018-11-04 16:44:07 -0800

    Move prototypes to _Prototypes folder

---
+++ _Prototypes/LZ_Clicker/LZ Clicker/Assets/ParticleClicking.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleClicking : MonoBehaviour {
+	public enum ParticleType 
+	{
+		CosmicRay, Contaminant, Neutron
+	}
+	public ParticleType mode;
+	public float particleSpeed=1.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	void OnMouseDown() {
+		Destroy(gameObject);
+	}
+
+	public void SetVelocity(Vector3 InitialVelocity){
+		//Takes in a Normalized velocity and applies it to the particle
+		gameObject.GetComponent<Rigidbody2D>().velocity=InitialVelocity*particleSpeed;
+	}
+}
