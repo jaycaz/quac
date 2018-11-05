56b47e0fa88b468a099718f593f862eba4282a9d
Ryan Linehan <ryanlinehan@Ryans-MacBook-Pro.local>
2018-11-04 18:34:44 -0800

    First commit. Beam pipe with motion and lighting created in scene.

---
+++ ParticleRunner/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+
+    public float bunchSpeed = 10.0f; //Speed of the pipe relative to the electron bunch
+    private GameObject beamPipe;
+
+    // Use this for initialization
+    void Start () {
+
+        //Get objects from the scene
+        beamPipe = GameObject.Find("BeamPipe");
+
+
+
+        StartBeamPipeMotion(beamPipe);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //Set the velocity of the beam pipe to the public variable above
+    void StartBeamPipeMotion(GameObject beamPipe){
+        Vector3 bunchVel = new Vector3(0f, 0f, -1*bunchSpeed);
+        beamPipe.GetComponent<Rigidbody>().velocity = bunchVel;
+    }
+}
