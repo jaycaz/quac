4214e8565f162eff956311f106fe404be7d96f37
eddbonilla <edgard@stanford.edu>
2018-11-04 22:17:14 -0800

    eventManager added

---
+++ ParticleRunner/Assets/Scripts/EventManager.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EventManager : MonoBehaviour {
+
+    private Dictionary <string, UnityEvent> eventDictionary; //Contains a list of the different triggers, indexed by names 
+
+    private static EventManager eventManager; 
+
+    public static EventManager instance //The instance of the event manager is set in this way to make sure there is only one such thing in the whole game, just in case
+    {
+        get
+        {
+            if (!eventManager)
+            {
+                eventManager = FindObjectOfType (typeof (EventManager)) as EventManager;
+
+                if (!eventManager)
+                {
+                    Debug.LogError ("There needs to be one active EventManger script on a GameObject in your scene.");
+                }
+                else
+                {
+                    eventManager.Init (); 
+                }
+            }
+
+            return eventManager;
+        }
+    }
+
+    void Init () //Initializes the dictionary of events
+    {
+        if (eventDictionary == null)
+        {
+            eventDictionary = new Dictionary<string, UnityEvent>(); 
+        }
+    }
+
+    public static void StartListening (string eventName, UnityAction listener) //Appends a new listener to an event, creates new event if none exists
+    {
+        UnityEvent thisEvent = null;
+        if (instance.eventDictionary.TryGetValue (eventName, out thisEvent))
+        {
+            thisEvent.AddListener (listener);
+        } 
+        else
+        {
+            thisEvent = new UnityEvent ();
+            thisEvent.AddListener (listener);
+            instance.eventDictionary.Add (eventName, thisEvent);
+        }
+    }
+
+    public static void StopListening (string eventName, UnityAction listener) // Removes a listener from an event. It is a good idea to run this on disable for the subscribers
+    {
+        if (eventManager == null) return;
+        UnityEvent thisEvent = null;
+        if (instance.eventDictionary.TryGetValue (eventName, out thisEvent))
+        {
+            thisEvent.RemoveListener (listener);
+        }
+    }
+
+    public static void TriggerEvent (string eventName) // Triggering the events, checkpoints, magnets etc. 
+    {
+        UnityEvent thisEvent = null;
+        if (instance.eventDictionary.TryGetValue (eventName, out thisEvent))
+        {
+            thisEvent.Invoke ();
+        }
+    }
+}
