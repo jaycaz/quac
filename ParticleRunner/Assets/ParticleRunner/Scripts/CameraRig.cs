5191fa9bdad218af547f3c8e7081720358b02576
Jordan Cazamias <jacazamias@gmail.com>
2018-12-11 00:12:38 -0800

    Pipe updates, camera rig prototype 1

---
+++ ParticleRunner/Assets/ParticleRunner/Scripts/CameraRig.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRig : MonoBehaviour {
+
+	public float m_moveAmount = 0.2f;
+	public float m_moveTime = 0.2f;
+	public float m_rotateTime = 0.2f;
+
+	public Transform m_leftPos;
+	public Transform m_rightPos;
+	public Transform m_forwardPos;
+	public Transform m_backPos;
+
+	private Vector3 m_startPos;
+	private Vector3 m_velocity;
+
+	void Start () {
+		m_startPos = transform.position;
+	}
+
+	void Update () {
+
+		Vector3 targetPos = m_startPos;
+		Quaternion targetRot = Quaternion.identity;
+		if(Input.GetKey(KeyCode.UpArrow))
+		{
+			targetPos = m_forwardPos.position;
+			targetRot = m_forwardPos.rotation;
+		}
+		if(Input.GetKey(KeyCode.DownArrow))
+		{
+			targetPos = m_backPos.position;
+			targetRot = m_backPos.rotation;
+		}
+		if(Input.GetKey(KeyCode.LeftArrow))
+		{
+			targetPos = m_leftPos.position;
+			targetRot = m_leftPos.rotation;
+		}
+		if(Input.GetKey(KeyCode.RightArrow))
+		{
+			targetPos = m_rightPos.position;
+			targetRot = m_rightPos.rotation;
+		}
+
+		transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref m_velocity, m_moveTime);
+		float rotAngle = Quaternion.Angle(transform.rotation, targetRot);
+		transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRot, rotAngle * m_rotateTime);
+	}
+}
---
+++ ParticleRunner/Assets/ParticleRunner/Scripts/SineMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SineMovement : MonoBehaviour {
+
+	public Vector3 m_moveAxis = Vector3.up;
+	public float m_angle = 0f;
+	public bool m_randomAxis = false;
+	public float m_amplitude = 1f;
+	public float m_speed = 1f;
+
+	private Vector3 m_startPos;
+	private float m_t;
+
+	void Start () {
+		m_startPos = transform.position;
+
+		if(m_randomAxis)
+		{
+			Vector2 r = Random.insideUnitCircle;
+			m_moveAxis = new Vector3(r.x, r.y, 0f);
+		}
+	}
+	
+	void Update () {
+		m_t += m_speed * 2.0f * Mathf.PI * Time.deltaTime;
+		transform.position = m_startPos + (m_moveAxis * m_amplitude * Mathf.Cos(m_t));
+	}
+}
