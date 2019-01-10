c6418194711e11b25965dc26b0151248d115cb8f
Jordan Cazamias <jacazamias@gmail.com>
2019-01-10 00:47:03 -0800

    Setup for particle switching
    [TODO] Pick colors for particle variants

--- ParticleRunner/Assets/ParticleRunner/Scripts/ParticleSwitcher.cs
+++ ParticleRunner/Assets/ParticleRunner/Scripts/ParticleSwitcher.cs
@@ -7,39 +7,9 @@ public class ParticleSwitcher : MonoBehaviour {
 	public List<GameObject> m_particlePrefabList;
 	public GameObject m_particleRoot;
 
-	void OnGUI()
-	{
-		// // GUILayout.Label(
-		// // "Enter - Next Pipe,\n" + 
-		// // "Backspace - Prev Pipe\n" + 
-		// // "Number Key - Select Pipe (1-9)\n");
-
-		// GUILayout.Label("Number Key - Select Pipe (1-9)\n");
-
-		// for(int i = 0; i < m_pipePrefabList.Count; i++)
-		// {
-		// 	string entryName = (i < m_names.Count) ? m_names[i] : m_pipePrefabList[i].name;
-		// 	GUILayout.Label(string.Format(
-		// 		"{0} - {1}",
-		// 		i+1,
-		// 		entryName
-		// 	));
-		// }
-	}
-
 	// Update is called once per frame
 	void Update () {
 
-		// if(Input.GetKeyDown(KeyCode.Return))
-		// {
-		// 	m_currentIndex = (m_currentIndex + 1) % m_pipePrefabList.Count;
-		// 	SwitchAllPipes(m_currentIndex);
-		// }
-		// if(Input.GetKeyDown(KeyCode.Backspace))
-		// {
-		// 	m_currentIndex = (m_currentIndex - 1 + m_pipePrefabList.Count) % m_pipePrefabList.Count;
-		// 	SwitchAllPipes(m_currentIndex);
-		// }
 		if(Input.GetKeyDown(KeyCode.Alpha1))
 			SwitchAllParticles(0);
 		if(Input.GetKeyDown(KeyCode.Alpha2))
@@ -58,7 +28,6 @@ public class ParticleSwitcher : MonoBehaviour {
 			SwitchAllParticles(7);
 		if(Input.GetKeyDown(KeyCode.Alpha9))
 			SwitchAllParticles(8);
-
 	}
 
 	public void SwitchAllParticles(int index)
@@ -69,15 +38,18 @@ public class ParticleSwitcher : MonoBehaviour {
 		}
 	}
 
-	public void SwitchAllParticles(GameObject newPipePrefab)
+	public void SwitchAllParticles(GameObject newParticlePrefab)
 	{
 		// Find all particle objects and replace them
 		int numParticles = m_particleRoot.transform.childCount;
-		Electron[] originalPipes = m_particleRoot.GetComponentsInChildren<Electron>();
+		Electron[] originalParticles = m_particleRoot.GetComponentsInChildren<Electron>();
 
-		for(int i = originalPipes.Length - 1; i >= 0; i--)
+		for(int i = originalParticles.Length - 1; i >= 0; i--)
 		{
-			GameObject.Destroy(originalPipes[i].gameObject);
+			Electron p = originalParticles[i];
+			GameObject newParticle = GameObject.Instantiate(newParticlePrefab, p.transform.position, p.transform.rotation);
+			newParticle.transform.parent = m_particleRoot.transform;
+			GameObject.Destroy(originalParticles[i].gameObject);
 		}
 	}
 }
---
+++ ParticleRunner/Assets/ParticleRunner/Scripts/WiggleChildObjects.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WiggleChildObjects : MonoBehaviour {
+
+	public float m_angle = 0f;
+	public float m_minAmplitude = 0.5f;
+	public float m_maxAmplitude = 1f;
+	public float m_minSpeed = 0.5f;
+	public float m_maxSpeed = 1f;
+
+	private struct MoveData
+	{
+		public Vector3 startPos;
+		public Vector3 moveAxis;
+		public float amplitude;
+		public float speed;
+		public float time;
+	}
+
+	private List<MoveData> m_movement;
+
+	void Start () {
+		m_movement = new List<MoveData>();
+		
+		for(int i = 0; i < transform.childCount; i++)
+		{
+			Transform t = transform.GetChild(i);
+
+			MoveData m = new MoveData();
+			m.time = 0f;
+			m.speed = Random.Range(m_minSpeed, m_maxSpeed);
+			m.amplitude = Random.Range(m_minAmplitude, m_maxAmplitude);
+			Vector2 r = Random.insideUnitCircle;
+			m.startPos = t.position;
+			m.moveAxis = new Vector3(r.x, r.y, 0f);
+
+			m_movement.Add(m);
+		}
+	}
+	
+	void Update () {
+
+		for(int i = 0; i < m_movement.Count; i++)
+		{
+			MoveData m = m_movement[i];
+
+			if(i < transform.childCount)
+			{
+				GameObject obj = transform.GetChild(i).gameObject;
+
+				if(obj != null)
+				{
+					// Update position
+					m.time += (m.speed * 2.0f * Mathf.PI * Time.deltaTime);
+					obj.transform.position = m.startPos + 
+						(m.moveAxis * m.amplitude * Mathf.Cos(m.time));
+				}
+			}
+
+			m_movement[i] = m;
+		}
+	}
+}
